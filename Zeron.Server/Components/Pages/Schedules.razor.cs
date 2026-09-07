738d51dded6bb9e443b7ef44d25019524e66d15f
jiowcl <jiowcl@gmail.com>
2026-09-07 18:39:37 +0800

    Improved Tasks / Schedules Table

--- Zeron.Server/Components/Pages/Schedules.razor.cs
+++ Zeron.Server/Components/Pages/Schedules.razor.cs
@@ -1,7 +1,6 @@
 // Zeron - Scheduled Task Application for Windows OS
 // Copyright (c) 2019 Jiowcl. All rights reserved.
 
-using Zeron.Server.ZServers;
 using Zeron.ZCore.Type;
 
 namespace Zeron.Server.Components.Pages
@@ -14,13 +13,34 @@ namespace Zeron.Server.Components.Pages
         // Schedules.
         private List<TaskScheduleInfoType> m_Schedules = [];
 
+        // Busy.
+        private bool m_IsBusy;
+
         /// <summary>
         /// OnInitializedAsync
         /// </summary>
         /// <returns>Returns Task.</returns>
         protected override async Task OnInitializedAsync()
         {
-            m_Schedules = await TaskScheduleServer.GetSchedulesAsync();
+            await ReloadAsync();
+        }
+
+        /// <summary>
+        /// ReloadAsync
+        /// </summary>
+        /// <returns>Returns Task.</returns>
+        private async Task ReloadAsync()
+        {
+            m_IsBusy = true;
+
+            try
+            {
+                m_Schedules = await TaskScheduleServer.GetSchedulesAsync();
+            }
+            finally
+            {
+                m_IsBusy = false;
+            }
         }
     }
 }
